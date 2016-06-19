cb1616913053b2008e29c0aa12f910b25c8fc5af
ocket8888 <ocket8888@gmail.com>
2016-06-18 18:12:54 -0600

    did some daemonization work, still getting invalid_auth for some reason

--- modules/slack/slack.py
+++ modules/slack/slack.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 from slacker import Slacker
 
-secretkey = str(open('slackbot/secret.txt').read());
-
+secretkey = str(open('/etc/slackbot/API').read());
+print("secretkey is {0}".format(secretkey));
 slack = Slacker(secretkey);
 
-#slack.chat.post_message('#general', 'Leif is a big nerd');
+slack.chat.post_message('#general', 'Leif is a big nerd');
