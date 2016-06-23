c455cd3db6507d62a5fe0929ff9719cc04f77fe3
ocket8888 <ocket8888@gmail.com>
2016-06-23 16:52:12 -0600

    Did some work on the install script

--- install.sh
+++ install.sh
@@ -1,9 +1,37 @@
 #!/bin/sh
 
+#Command-line options
+IFLAG=false
+SFLAG=false
+
+#get options
+while getopts his option
+do	case "$option" in
+	h)
+		echo "Usage: install.sh [-his]"
+		echo "======= Options ========"
+		echo "h : Display this help text and exit."
+		echo "i : Install dependancies (will attempt to determine your package manager if necessary, but yum is NOT supported)."
+		echo "s : systemd-less install (bot daemon will be placed in /var/www/botd)."
+		exit 0
+		;;
+	i)
+		IFLAG=true
+		;;
+	s)
+		SFLAG=true
+		;;
+	[?])
+		echo "Try 'install.sh -h' for help"
+		exit 1
+		;;
+	esac
+done
+
 #check for proper permissions
 if [ $(id -u) -ne 0 ]
 then
-	if [ $DISPLAY && $(which gksudo) ]
+	if [ $DISPLAY ] && [ $(which gksudo) ]
 	then
 		gksudo "$0"
 		exit 0
@@ -13,25 +41,116 @@ then
 		sudo "$0"
 		exit 0
 	fi
-	echo "This script must be run with root permissions, and you don't appear to have sudo or gksudo installed, so we can't do it ourselves"
+	echo "This script must be run with root permissions, and you don't appear to have sudo so we can't do it ourselves"
+	exit 2
 fi
 
-#List of dependancies we need to install
-PYTHON = true
-PYTHONVERSION = true
-SYSTEMD = true
-PIP = true
+
+#if -i is specified, we must find a package manager
+if [ $IFLAG == true ]
+then
+	if [ $(which apt-get) ]
+	then
+		PKG=apt
+	elif [ $(which pacman) ]
+	then
+		PKG=pac
+	else
+		echo "-i option was specified, but a suitable package manager could not be found."
+		echo "Either install dependancies manually, or continue without the -i option."
+		exit 4
+	fi
+fi
+	
 
 
+echo "Checking for dependancies..."
 
-#check for python version 3
+#####################################
+###	CHECK FOR DEPENDANCIES    ###
+#####################################
+
+#$PATH/python -> */python3
 if [ ! $(which python) ]
 then
-	PYTHON = false
+	if [ $(which python3) ]
+	then
+		ln -s /usr/bin/python $(which python3)
+	elif [ $IFLAG == false ]
+	then
+		echo "You do not appear to have python in your `$PATH`, if you would like us to try to install it run 'install.sh -i', otherwise you must either install it yourself or ensure that it is in your `$PATH`."
+		exit 3
+	elif [ $PKG == apt ]
+	then
+		apt-get install python3 && ln -s /usr/bin/python /usr/bin/python3
+	elif [ $PKG == pac ]
+	then
+		pacman -S python3 && ln -s /usr/bin/python /usr/bin/python3
+	fi
 fi
 
+echo "\tPython found..."
+
+
+#python is version 3+
 if [ $(python --version | sed 's/.*\s//' | sed 's/\..*//') != 3 ]
 then
-	echo "Python appears to not be version 3+, either it is not installed or `$PATH/python` is not a symlink to it."
-	exit 1
+	if [ $IFLAG == true ]
+	then
+		echo "CAUTION: /usr/bin/python is being made a symlink to python3, if this was previously a symlink to python2 some of your scripts may break."
+		if [ $(which python3) ]
+		then
+			ln -s /usr/bin/python $(which python3)
+		elif [ $PKG == apt ]
+		then
+			apt-get install python3 && ln -s /usr/bin/python /usr/bin/python3
+		elif [ $PKG == pac ]
+		then
+			pacman -S python3 && ln -s /usr/bin/python /usr/bin/python3
+		fi
+	else
+		echo "Python is not version 3+, you must link /usr/bin/python to a python3 executable, install python3 and then link, or use the '-i' option."
+		exit 3
+	fi
 fi
+
+echo "\tPython is version 3+..."
+
+
+#pip is version 3+
+if [ ! $(which pip3) ]
+then
+	if [ $IFLAG == true ]
+	then
+		if [ $PKG == apt ]
+		then
+			apt-get install python-pip3
+		elif [ $PKG == pac ]
+			pacman -S python-pip3
+		fi
+	else
+		echo "Pip version 3+ could not be found, you must install pip3 or use the '-i' option."
+		exit 3
+	fi
+fi
+
+echo "\tPip version 3+ found..."
+
+
+#pip is version 3+
+if [ ! -d /lib/systemd/system/ ]
+then
+	if [ $SFLAG == false ]
+	then
+		echo "You do not have systemd, if you wish to install anyway, run this script with the '-s' option"
+		echo "NOTE: bot daemon will be placed in /var/www/slackbot/botd"
+		exit 3
+	fi
+	SYSTEMD=false
+	echo "Systemd not detected... " >2
+fi
+
+echo "Done."
+
+
+
