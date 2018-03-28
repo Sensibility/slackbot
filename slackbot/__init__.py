208f2807461e9dea4d4a92de5aa03cd7027094a0
ocket8888 <ocket8888@gmail.com>
2018-03-28 13:44:35 -0600

    Removed Daemonization, formatting update

--- slackbot/__init__.py
+++ slackbot/__init__.py
@@ -1,37 +1,48 @@
 #!/usr/bin/python3
+"""
+This package contains all the functionality necessary to create a slack chatbot.
+"""
 
-from slacker import Slacker
 
-from pwd import getpwnam
+def main() -> int:
+	"""
+	Starts up the slackbot, initializing any installed extension modules.
+	"""
+	import sys
+	import slacker
+	from . import commander
 
-from commander import *
+	print("Slackbot starting up... ");
 
-print("Slackbot starting up... ");
+	try:
+		slackbot = slacker.Slacker(open('/etc/slackbot/API').read().strip());
+	except OSError as e:
+		print("Could not read API token file: %s" % e, file=sys.stderr)
+		return 1
 
-slackbot = Slacker(open('/etc/slackbot/API').read().strip());
+	users = slackbot.users.list().body['members']
 
-users = slackbot.users.list().body['members']
+	listenChannels = []
+	lastheard = {}
 
-listenChannels = []
-lastheard = dict()
+	for channel in slackbot.channels.list().body['channels']:
+		if channel['is_member']:
+			listenChannels.append(channel)
+			lastheard[channel['id']] = -1.0
 
-for channel in slackbot.channels.list().body['channels']:
-	if channel['is_member']:
-		listenChannels.append(channel)
-		lastheard[channel['id']] = -1.0
+	while(True):
+		for channel in listenChannels:
+			chat_hist = slackbot.channels.history(channel['id']).body['messages']
+			for message in chat_hist:
+				if lastheard[channel['id']] == float(message['ts']):
+					break
+				if "bot_id" not in message and 'text' in message:
+					ts = float(message['ts'])
+					if lastheard[channel['id']] <= ts:
+						lastheard[channel['id']] = ts
+						response = commander.parseMessage(message, slackbot, channel, users)
+						if response != None:
+							print(response)
 
-while(True):
-	for channel in listenChannels:
-		chat_hist = slackbot.channels.history(channel['id']).body['messages']
-		for message in chat_hist:
-			if "bot_id" in message:
-				continue
-			if lastheard[channel['id']] == float(message['ts']):
-				break
-			if 'text' in message:
-				if lastheard[channel['id']] > float(message['ts']):
-					continue
-				lastheard[channel['id']] = float(message['ts'])
-				response = parseMessage(message, slackbot, channel, users)
-				if response != None:
-					print(response)
+if __name__ == '__main__':
+	exit(main())
